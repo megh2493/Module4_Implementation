be30478b03bc1232a58c79d58ea0267c6c2df795
megh2493 <mmadhusu@usc.edu>
2018-06-02 13:36:08 -0700

    Changes made in javascript

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-(function() {
-  // *******************************
+// *******************************
 // START HERE IF YOU WANT A MORE CHALLENGING STARTING POINT FOR THIS ASSIGNMENT
 // *******************************
 //
@@ -35,38 +34,37 @@ WARNING!!! WARNING!!!
 // See Lecture 52, part 2
 // (Note, Step 2 will be done in the SpeakHello.js file.)
 
-var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+(function(){
 
-// STEP 10:
-// Loop over the names array and say either 'Hello' or "Good Bye"
-// using the 'speak' method or either helloSpeaker's or byeSpeaker's
-// 'speak' method.
-// See Lecture 50, part 1
- for (var i=0; i < names.length; i++) {
+	var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
-     if ( names[i].charAt(0).toLowerCase() === "j") {
-      byeSpeaker.speak(names[i]);
-    } else {
-      helloSpeaker.speak(names[i]);
-    }
-   
-  }
+	// STEP 10:
+	// Loop over the names array and say either 'Hello' or "Good Bye"
+	// using the 'speak' method or either helloSpeaker's or byeSpeaker's
+	// 'speak' method.
+	// See Lecture 50, part 1
+	for (var name in names) {
 
+		// STEP 11:
+		// Retrieve the first letter of the current name in the loop.
+		// Use the string object's 'charAt' function. Since we are looking for
+		// names that start with either upper case or lower case 'J'/'j', call
+		// string object's 'toLowerCase' method on the result so we can compare
+		// to lower case character 'j' afterwards.
+		// Look up these methods on Mozilla Developer Network web site if needed.
+		var firstLetter = name.charAt[0].toLowerCase;
+		byeSpeaker.speak(name);
 
-  // STEP 11:
-  // Retrieve the first letter of the current name in the loop.
-  // Use the string object's 'charAt' function. Since we are looking for
-  // names that start with either upper case or lower case 'J'/'j', call
-  // string object's 'toLowerCase' method on the result so we can compare
-  // to lower case character 'j' afterwards.
-  // Look up these methods on Mozilla Developer Network web site if needed.
-  // var firstLetter =
-
-  // STEP 12:
-  // Compare the 'firstLetter' retrieved in STEP 11 to lower case
-  // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
-  // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
-  // name in the loop.
-
-
-})();
+		// STEP 12:
+		// Compare the 'firstLetter' retrieved in STEP 11 to lower case
+		// 'j'. If the same, call byeSpeaker's 'speak' method with the current name
+		// in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
+		// name in the loop.
+		if (if firstLetter === "j") {
+				byeSpeaker.speak(name);
+			}
+			else {
+				helloSpeaker.speak(name);
+			}
+		}
+});
